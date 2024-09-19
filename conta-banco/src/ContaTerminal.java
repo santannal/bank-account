40320d5ebe9d15204ea84f84d512b5c39d41690f
Leonardo Sant'Anna <140214338+santannal@users.noreply.github.com>
2024-09-19 11:49:15 -0300

    adicionando desafio

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -2,21 +2,25 @@ import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        
+        // Conhecer e importar a classe scanner
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Digite seu nome: ");
-        String name = scanner.next();
+        // Exibir mensagens para a inserção de dados
 
-        System.out.println("Digite sua idade: ");
-        int idade = scanner.nextInt();
+        // Obter os dados
+        System.out.println("Por favor digite o número da conta: ");
+        int numAgencia = scanner.nextInt();
 
-        System.out.println("Digite sua altura: ");
-        double altura = scanner.nextDouble();
+        System.out.println("Digite sua agência: ");
+        String agencia = scanner.next();
 
-        System.out.println("Digite seu peso: ");
-        double peso = scanner.nextDouble();
+        System.out.println("Digite o nome do cliente: ");
+        String nome = scanner.next();
 
-        System.out.println(name + " possui " + idade + " anos, com uma altura de: " + altura + " e peso de: " + peso);
+        System.out.println("Digite o saldo: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia
+                + ", conta " + numAgencia + " e seu saldo " + saldo + " já está disponível para saque");
     }
 }
